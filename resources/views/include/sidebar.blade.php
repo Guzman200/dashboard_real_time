4b87b1e5bb0018c4dbf0f25cb5d8350532a97e02
Pedro Guzman <my.rg.developer@gmail.com>
2022-05-18 23:39:26 -0500

    Se modifico navbar

---
+++ resources/views/include/sidebar.blade.php
@@ -0,0 +1,48 @@
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+        data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon class
+        with font-awesome or any other icon font library -->
+
+        <li class="nav-item">
+            <a href="/" class="nav-link">
+                <i class="nav-icon fas fa-tachometer-alt"></i>
+                <p>
+                    Dashboard
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="./empleados" class="nav-link">
+                <i class="nav-icon fas fa-user-tie"></i>
+                <p>
+                    Empleados
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="./clientes" class="nav-link">
+                <i class="nav-icon fas fa-user"></i>
+                <p>
+                    Clientes
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="./sucursales" class="nav-link">
+                <i class="nav-icon fas fa-store-alt"></i>
+                <p>
+                    Sucursales
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="./areas" class="nav-link">
+                <i class="nav-icon fas fa-store-alt"></i>
+                <p>
+                    Areas
+                </p>
+            </a>
+        </li>
+    </ul>
+</nav>
